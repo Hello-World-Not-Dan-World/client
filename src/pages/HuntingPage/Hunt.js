5ebf8fea305c6281ea591879ccca3befc802ef4a
mynameSumin <msm4167@naver.com>
2024-05-11 06:14:11 +0900

    hunt 페이지 완성

--- src/pages/HuntingPage/Hunt.js
+++ src/pages/HuntingPage/Hunt.js
@@ -1,7 +1,16 @@
 import "./Hunt.css";
+import video from "../../assert/cleaning2.gif";
+import { useNavigate } from "react-router-dom";
 
 export default function Hunt(){
+    let navigate = useNavigate();
     return (
-        <div class = "main">사냥 중...</div>
+        <div className = "main">
+            <div className = "phone">
+                <h1>민수민 님이 사냥 중입니다!</h1>
+                <img className = "cleaning" src={video}/>
+                <button className="complete" onClick={()=>{navigate('/map')}}>사냥 완료!</button>      
+            </div> 
+        </div>
     )
 }
