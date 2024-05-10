d2150296c482458026d39079566c6f87e74e227c
mynameSumin <msm4167@naver.com>
2024-05-11 01:19:39 +0900

    지도 및  marker 표시

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Map from './component/Map.js';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" component={<div></div>} />
+        <Route path="/map" component={<Map/>}/>
+      </Routes>
+      <Map/>
     </div>
   );
 }
